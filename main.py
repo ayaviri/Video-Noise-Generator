1c555e42fc102790b096e0b5ed3c811b523e8ab3
ayaviri <joshuayaviri@gmail.com>
2021-11-13 18:14:52 -0500

    Generates a 10-second video of random noise

---
+++ main.py
@@ -0,0 +1,29 @@
+import numpy
+import random
+import moviepy.editor as mpy
+import datetime
+#from Google import Create_Service
+#from googleapiclient.http import MediaFileUpload
+
+SURFACEWIDTH = 1920
+SURFACEHEIGHT = 1080
+
+# returns a numpy array which represents a single frame
+def make_np_array(t):
+  frame = []
+  for i in range(SURFACEHEIGHT):
+    currentRow = []
+    for j in range(SURFACEWIDTH):
+      currentPixel = []
+      for k in range(3):
+        currentPixel += [random.randint(0, 255)]
+      currentRow += [currentPixel]
+    frame += [currentRow]
+
+  array = numpy.array(frame)
+  return array
+
+if __name__ == '__main__':
+  video = mpy.VideoClip(make_np_array, duration = 10)
+  video.write_videofile("test.mp4", fps = 10, audio = False, preset = "ultrafast", logger = "bar" )
+
